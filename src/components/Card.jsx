bc3c17f55d013fdde522433e7f9287b41251ed59
jackerekson <erekson.jack@gmail.com>
2021-11-08 15:36:25 -0600

    adding changes to card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,6 +6,14 @@ const Card = ({ img, body, heading, alt}) => {
     const[paragraph, setParagraph] = useState(1)
     const[length, setLength] = useState('short')
     const[title, setTitle] = useState('')
+    
+    const handleClick = async() => {
+        const res = await axios.get(`https://loripsum.net/api/${paragraph}/${length}/plaintest`)
+        setCardBody(res.data)
+        setTitle(makeTitle)
+        sendLorem()
+    }
+    
     let arr = ['Lorem', 'Ipsum', 'Dolor', 'Sit', 'Amet', 'Consectetur', 'Adispiscing', 'Elit', 'At', 'Verte', 'Gravius', 'Mihi', 'Inquam', 'Qui', 'Te', 'Id', 'Ipsum', 'Rogavi', 'Vide', 'Quantum']
     const rando = arr[Math.floor(Math.random()*arr.length)]
     const rando2 = arr[Math.floor(Math.random()*arr.length)]
@@ -13,13 +21,6 @@ const Card = ({ img, body, heading, alt}) => {
     titleArr.push(rando)
     titleArr.push(rando2)
     const makeTitle = titleArr.join(' ')
-
-    const handleClick = async() => {
-        const res = await axios.get(`https://loripsum.net/api/${paragraph}/${length}/plaintest`)
-        setCardBody(res.data)
-        setTitle(makeTitle)
-        sendLorem()
-    }
 
     const sendLorem = () => {
         axios.post('http://localhost:3001/lorem', {cardBody, title})
